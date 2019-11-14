30140f7fc52cc2ddbbc502ca288ceedaf63c0e3c
Kenneth Siu <kenneth.siu@softwire.com>
2019-11-14 12:23:35 +0000

    Add starting example

--- src/training/sandbox/Main.java
+++ src/training/sandbox/Main.java
@@ -7,7 +7,10 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+        String[] zoo = new String[] { "Elephant", "Ostrich", "Zebra", "Triceratops" };
 
+        UI.sortZoo(zoo);
+        UI.display(zoo);
     }
 }
 
