49248edf84f2f21760aca184e1d852603573ff6d
Kenneth Siu <kenneth.siu@softwire.com>
2019-11-20 17:40:01 +0000

    Inimitable four, part 2b

--- src/training/sandbox/animal/Animal.java
+++ src/training/sandbox/animal/Animal.java
@@ -2,10 +2,12 @@ package training.sandbox.animal;
 
 public class Animal {
 
+    public int age;
+
     { System.out.println("Bow wow");}
     static { System.out.println("Animal"); }
 
-    public void Animal() {
+    public Animal(int age) {
         System.out.println("I am an animal.");
     }
 }
--- src/training/sandbox/animal/Cat.java
+++ src/training/sandbox/animal/Cat.java
@@ -2,10 +2,17 @@ package training.sandbox.animal;
 
 public class Cat extends Animal {
 
+    public String name;
+
     { System.out.println("Meow"); }
     static { System.out.println("Cat"); }
 
     public Cat() {
+        this(4);
+    }
+
+    public Cat(int age) {
+        super(age);
         System.out.println("I am a cat.");
     }
 }
